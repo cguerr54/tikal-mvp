a40466628fc9942838d855ef671fd69b6cd33968
Kari <kouglas2@gmail.com>
2023-06-28 18:29:06 -0500

    fix style/ add one color to tailwind.config

--- tikal-mvp/src/app/page.tsx
+++ tikal-mvp/src/app/page.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 export default function Login() {
   return (
     <main className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <section className=" sm:mx-auto sm:w-full sm:max-w-sm border-pink-500">
+      <section className="sm:mx-auto sm:w-full sm:max-w-sm border-pink-500">
         <div>
-          <div className="border-2  border-green-400">
+          <div className="border border-green-400">
             <Image
               src="/assets/tikal-logo.png"
               alt='skyblue logo for tikal industries'
@@ -14,9 +14,9 @@ export default function Login() {
               height={400}
               // className='mx-auto h-10 w-auto' not sure if this looks better or the set height width
             />
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
           </div>
-          <div className=" mt-10 sm:mx-auto sm:w-full sm:max-w-sm  border-dotted border-2 border-blue-700">
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm  border-dotted border-2 border-blue-700">
             <form className='space-y-6' action="#" method='POST' >
               <div>
                 <label className='block text-sm font-medium leading-6 text-gray-900' htmlFor="email">Email Address</label>
@@ -25,14 +25,18 @@ export default function Login() {
                 </div>
               </div>
               <div>
-                <div className='flex items-center justify-between'>
-                  <label htmlFor="password" className='block text-sm font-medium leading-6 text-gray-900'>Password</label>
+                <div className=' items-center justify-between'>
+                  <label htmlFor="password" className='block text-sm font-medium leading-6 text-gray-900 '>Password</label>
                   <div className='text-sm'>
                     {/* change the text color to match the logo */}
-                    <a href="#" className='font-semibold text-blue-300 hover:text-indigo-500'>Forgot your password</a>
-                  </div>
-                  <div></div>
+                    <a href="#" className='font-semibold text-tikal-cyan hover:text-indigo-500 float-right -mt-6 '>Forgot your password</a>
 
+                    <div className=''>
+                      <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+
+                    </div>
+                  </div>
+                  
                 </div>
               </div>
                 <input 
