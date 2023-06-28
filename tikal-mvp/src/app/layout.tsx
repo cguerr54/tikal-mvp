4338d4b4182cfdc185903e387fbf41763eaee61a
Kari <kouglas2@gmail.com>
2023-06-27 19:19:12 -0500

    add style to login

--- tikal-mvp/src/app/layout.tsx
+++ tikal-mvp/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html className="h-full"   lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
--- tikal-mvp/src/app/page.tsx
+++ tikal-mvp/src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Login() {
   return (
-    <main className="">
-      <section className=" w-3/5 m-auto  border-2 border-pink-500">
+    <main className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <section className=" sm:mx-auto sm:w-full sm:max-w-sm border-pink-500">
         <div>
           <div className="border-2  border-green-400">
             <Image
@@ -12,22 +12,29 @@ export default function Login() {
               alt='skyblue logo for tikal industries'
               width={400}
               height={400}
+              // className='mx-auto h-10 w-auto' not sure if this looks better or the set height width
             />
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
           </div>
-          <div className="  border-dotted border-2 border-blue-700">
-            <form action="">
-              <label className="text" for="text">username:</label>
-                <input 
-                  name="text" 
-                  type="text"
-                  title="username"
-                />
-              <label className="password" for="password">password:</label>
-                <input 
-                  name="password" 
-                  type="password"
-                  title="password"
-                />
+          <div className=" mt-10 sm:mx-auto sm:w-full sm:max-w-sm  border-dotted border-2 border-blue-700">
+            <form className='space-y-6' action="#" method='POST' >
+              <div>
+                <label className='block text-sm font-medium leading-6 text-gray-900' htmlFor="email">Email Address</label>
+                <div className='mt-2' >
+                  <input id='email' name='email' autoComplete='email' type="email" required className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'/>
+                </div>
+              </div>
+              <div>
+                <div className='flex items-center justify-between'>
+                  <label htmlFor="password" className='block text-sm font-medium leading-6 text-gray-900'>Password</label>
+                  <div className='text-sm'>
+                    {/* change the text color to match the logo */}
+                    <a href="#" className='font-semibold text-blue-300 hover:text-indigo-500'>Forgot your password</a>
+                  </div>
+                  <div></div>
+
+                </div>
+              </div>
                 <input 
                   type="checkbox"
                   name="checkbox" 
