e878596bbc725ea9f37e02e4615af6eca6c64173
Kari <kouglas2@gmail.com>
2023-06-28 18:39:12 -0500

    add submit button and other styles

--- tikal-mvp/src/app/page.tsx
+++ tikal-mvp/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Login() {
   return (
-    <main className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <main className="flex min-h-full flex-col justify-center px-6 py-28 lg:px-8">
       <section className="sm:mx-auto sm:w-full sm:max-w-sm border-pink-500">
         <div>
           <div className="border border-green-400">
@@ -19,17 +19,17 @@ export default function Login() {
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm  border-dotted border-2 border-blue-700">
             <form className='space-y-6' action="#" method='POST' >
               <div>
-                <label className='block text-sm font-medium leading-6 text-gray-900' htmlFor="email">Email Address</label>
+                <label className='block text-sm font-medium leading-6 text-gray-900' htmlFor="email">Email Address:</label>
                 <div className='mt-2' >
                   <input id='email' name='email' autoComplete='email' type="email" required className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'/>
                 </div>
               </div>
               <div>
                 <div className=' items-center justify-between'>
-                  <label htmlFor="password" className='block text-sm font-medium leading-6 text-gray-900 '>Password</label>
+                  <label htmlFor="password" className='block text-sm font-medium leading-6 text-gray-900 '>Password:</label>
                   <div className='text-sm'>
                     {/* change the text color to match the logo */}
-                    <a href="#" className='font-semibold text-tikal-cyan hover:text-indigo-500 float-right -mt-6 '>Forgot your password</a>
+                    <a href="#" className='font-semibold text-tikal-cyan hover:text-indigo-500 float-right -mt-6 '>Forgot your password?</a>
 
                     <div className=''>
                       <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
@@ -39,19 +39,15 @@ export default function Login() {
                   
                 </div>
               </div>
-                <input 
-                  type="checkbox"
-                  name="checkbox" 
-                  title="checkbox"
-                />
-              <label className="checkbox" htmlFor="checkbox">Remember me</label>
-          <div className="">
-                {/* needs to be a link as a button */}
-                <h1>Sign in</h1>  
-                {/* needs to be a link but just bolded */}
-                <p>Don&apos;t have an account?</p>
-          </div>
+              <div>
+                <button type="submit" className="flex w-full justify-center rounded-md bg-tikal-cyan px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+              </div>
+
             </form>
+            <p class="mt-10 text-center text-sm text-gray-500">
+              Not a member?
+              <a href="#" class="font-semibold leading-6 text-black hover:text-indigo-500"> Sign up for an account</a>
+    </p>
           </div>
         </div>
       </section>
