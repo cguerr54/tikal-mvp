549030ee30a0f86899c649308d5c352c35ea3be9
Kari <kouglas2@gmail.com>
2023-07-03 15:45:33 -0500

    add sidebar, && darkMode

--- tikal-mvp/src/app/dashboard-view/page.tsx
+++ tikal-mvp/src/app/dashboard-view/page.tsx
@@ -15,6 +15,12 @@ export default function Dashboard(){
     
       <section>
       {/*put DataViz component here  */}
+
+
+    {/*  */}
+
+
+
       </section>
     </main>
   )
--- tikal-mvp/src/components/SidebarNav.tsx
+++ tikal-mvp/src/components/SidebarNav.tsx
@@ -1,43 +1,51 @@
 //@ts-nocheck
-import { GoMarkGithub } from 'react-icons/go'
 import { CiCoffeeCup, CiBatteryFull, CiWheat } from 'react-icons/ci'
-// import { IoFlashlightOutline, IoFlashlight } from 'react-icons/io5'
-// import useDarkMode from '../hooks/useDarkMode';
+import { IoFlashlightOutline, IoFlashlight } from 'react-icons/io5'
+import useDarkMode from '../hooks/useDarkMode';
 // import { IconBaseProps } from 'react-icons';
 
 
 export default function SidebarNav() {
 
-  const SideBarIcon = ({ icon }) => (
-    <div className="sidebar-icon">
+  const SideBarIcon = ({ icon, text = 'tooltip 🚨' }) => (
+    <div className="sidebar-icon group" >
       {icon}
+      <span class='sidebar-tooltip group-hover:scale-100'>
+        {text}
+      </span>
     </div>
   )
 
-//   const SwitchTheme = () => {
-//     const [darkTheme, setDarkTheme] = useDarkMode()
-//     const handleMode = () => setDarkTheme(!darkTheme)
+  const SwitchTheme = () => {
+    const [darkTheme, setDarkTheme] = useDarkMode()
+    const handleMode = () => setDarkTheme(!darkTheme)
 
-//     return ( 
-//         <span onClick={handleMode}>
-//         {darkTheme ? (
-//                 <IoFlashlightOutline size='28' /> 
-//             ) : (
-//                 <IoFlashlight size='28' />
-//             )}
-//         </span>
-//     );
-// }
+    return ( 
+        <span onClick={handleMode}>
+        {darkTheme ? (
+                <IoFlashlightOutline size='28' /> 
+            ) : (
+                <IoFlashlight size='28' />
+            )}
+        </span>
+    );
+}
   
   return(
-    <div className="fixed h-screen w-16 flex flex-col bg-gray-400 text-white shadow-lg">
+    <div className="fixed h-screen w-16 flex flex-col bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white shadow-lg">
 
 <SideBarIcon icon={<CiCoffeeCup size="28" />} />
         <SideBarIcon icon={<CiBatteryFull size="28" />} />
         <SideBarIcon icon={<CiWheat size="28" />} />
         {/* <SideBarIcon icon={<GoMarkGithub size="28" />} /> */}
-        {/* <SideBarIcon icon={<SwitchTheme />} /> */}
+        <SideBarIcon icon={<SwitchTheme />} />
 
     </div>
   )
 }
+
+
+// ok technically you need to add the 
+// dark mode stuff to the navbar and 
+// the side bar using the class system
+// so just do that 
